101a59b30c5524015b03a162e7be6817f7c1fac3
XkenmaX <102683904+XkenmaX@users.noreply.github.com>
2022-04-20 10:38:43 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pagina.proyecto</title>
+    <link rel="stylesheet" href="/css/normalize.css">
+    <link rel="stylesheet" href="/css/index.css">
+    
+</head>
+<body>
+    <header class="hero">
+        <div class="container">
+            <nav class="nav">
+                <a class="nav__items" href="#">Servicio al cliente</a>
+                <a class="nav__items" href="#">Sobre la tarjeta</a>
+                <a class="nav__items nav__items--cta" href="#">inicio de sesion</a>
+            </nav>
+            
+            <section class="hero__container">
+                <div class="hero__texts">
+                    <h1 class="hero__title">Elemeto de titulo</h1>
+                    <h2 class="hero__subtitle">Adquiere tu tarjeta virtual </h2>
+                    <a href="#" class="hero__button">Mas info</a>
+                </div>
+
+            </section>
+
+        </div>
+        <div class="hero__wave" style="overflow: hidden;" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: white;"></path></svg></div>
+        
+    </header>
+    <main>
+        <section class="informacion container">
+            <img src="/img/pexels-monstera-5849559.jpg"  class="informacion__picture">
+            <h2 class="subtitle">Tarjeta virtual</h2>
+            <p class="informacion__copy"> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil, eum quo? Dolore ea delectus dolor tenetur quaerat magni eius consectetur beatae unde odit maxime repellat molestias, consequuntur similique quas possimus.</p>
+            <a href="#" class="informacion__button">Mas informacion</a>
+        </section>
+
+        <section class="about container">
+            <div class="about__text">
+                <h2 class="subtitle"> ¿Que es esta tarjeta virtual</h2>
+                <p class="about__paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti amet laborum velit culpa. Incidunt sit ipsum, laudantium, facilis, optio ipsa deserunt ducimus architecto assumenda distinctio omnis consequuntur. Optio, perferendis neque!</p>
+
+                <p class="about__paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti amet laborum velit culpa. Incidunt sit ipsum, laudantium, facilis, optio ipsa deserunt ducimus architecto assumenda distinctio omnis consequuntur. Optio, perferendis neque!</p>
+            </div>
+
+            <figure class="about__img about__img--left">
+                <img src="img/dev.svg.svg"  class="about__picture">
+            </figure>
+
+            <figure class="about__img">
+                <img src="/img/dev.svg2.svg" class="about__picture">
+            </figure>
+
+            <div class="about__text">
+                <h2 class="subtitle">Funcion de la tarjeta virtual</h2>
+                <p class="about__paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti amet laborum velit culpa. Incidunt sit ipsum, laudantium, facilis, optio ipsa deserunt ducimus architecto assumenda distinctio omnis consequuntur. Optio, perferendis neque!</p>
+
+                <p class="about__paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti amet laborum velit culpa. Incidunt sit ipsum, laudantium, facilis, optio ipsa deserunt ducimus architecto assumenda distinctio omnis consequuntur. Optio, perferendis neque!</p>
+            </div>
+        </section>
+
+        <!-- <section class="project">
+            <div class="container">
+                <h2 class="subtitle">Sobre nosotros</h2>
+                <div class="proyect__grid">
+                    <article class="proyect__item">
+                        <img src="/img/img1.jpg " class="projects__img" >
+                        <div class="projects__hover">
+                            <h2 class="projects__title">projecto</h2>
+                            <i class="far fa-file-alt projects__icon"></i>
+                        </div>
+                    </article>
+
+                    <article class="proyect__item">
+                        <img src="/img/img2.jpg" class="projects__img" >
+                        <div class="projects__hover">
+                            <h2 class="projects__title">projecto</h2>
+                            <i class="far fa-file-alt projects__icon"></i>
+                        </div>
+                    </article>
+
+                    <article class="proyect__item">
+                        <img src="/img/img3.jpg" class="projects__img"  >
+                        <div class="projects__hover">
+                            <h2 class="projects__title">projecto</h2>
+                            <i class="far fa-file-alt projects__icon"></i>
+                        </div>
+                    </article>
+
+                    <article class="proyect__item">
+                        <img src="/img/img4.jpg " class="projects__img"  >
+                        <div class="projects__hover">
+                            <h2 class="projects__title">projecto</h2>
+                            <i class="far fa-file-alt projects__icon"></i>
+                        </div>
+                    </article>
+
+                    <article class="proyect__item">
+                        <img src="/img/img5.jpg " class="projects__img"  >
+                        <div class="projects__hover">
+                            <h2 class="projects__title">projecto</h2>
+                            <i class="far fa-file-alt projects__icon"></i>
+                        </div>
+                    </article>
+                </div>
+            </div>
+        </section> -->
+        
+        <section class="testimony container">
+            <h2 class="sutitle">¿Que somo?</h2>
+
+            <div class="testimony__grid">
+                <article class="testimony__item">
+                    <div class="testimony__person">
+                        <img src="/img/pexels-ron-lach-8357683.jpg" alt="" class="testimony__img">
+                        <div class="testimony texts">
+                            <h3 class="testimony__name">
+                                <p class="testimony__verification">Programador</p>
+
+                            </h3>
+                        </div>
+                    </div>
+                    <p class="testimony__review">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum laudantium harum voluptatibus vero perferendis iusto natus dicta est fugit pariatur!</p>
+                </article>
+
+                <article class="testimony__item">
+                    <div class="testimony__person">
+                        <img src="/img/pexels-ron-lach-8357683.jpg" alt="" class="testimony__img">
+                        <div class="testimony texts">
+                            <h3 class="testimony__name">
+                                <p class="testimony__verification">Programador</p>
+
+                            </h3>
+                        </div>
+                    </div>
+                    <p class="testimony__review">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum laudantium harum voluptatibus vero perferendis iusto natus dicta est fugit pariatur!</p>
+                </article>
+            </div>
+
+        </section>
+    </main>
+
+    <footer class="footer">
+        <div class="container footer__grid">
+            <nav class="nav nav--footer">
+                <a class="nav__items nav__items--footer" href="">Inicio</a>
+                <a class="nav__items nav__items--footer" href="">Sobre la tarjeta</a>
+                <a class="nav__items nav__items--footer" href="">Proyectos</a>
+                <a class="nav__items nav__items--footer" href="">Servicio al cliente</a>
+            </nav>
+            <section class="footer__contact">
+                <h3 class="footer__title">Contactanos</h3>
+                <div class="footer__icons">
+                    <span class="footer__container-icons ">
+                        <a class="fab fa-brands fa-facebook-f footer__icon" href="#"></a>
+                    </span>
+
+                    <span class="footer__container-icons">
+                        <a class="fab fa-brands fa-twitter footer__icon" href="#"></a>
+                    </span>
+
+                    <span class="footer__container-icons">
+                        <a class="fab fa-brands fa-whatsapp footer__icon" href="#"></a>
+                    </span>
+
+            </section>
+
+        </div>
+    </footer>
+
+
+
+    <script src="https://kit.fontawesome.com/3057e5b6bc.js" crossorigin="anonymous"></script>
+</body>
+</html>
